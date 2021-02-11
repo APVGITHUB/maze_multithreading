02334f3348a1417e8f0fdef237dfb8b562ef0800
APVGITHUB <alex.pr434@gmail.com>
2021-02-11 13:36:15 +0100

    final

--- threading_map.cpp
+++ threading_map.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void threading_map(bool* cur_thread, int* next_y, int* next_x, vector<vector<char>> maze_map, int new_y, int new_x){
     
     // Search available path
-    if(maze_map[new_y][new_x] == ' ' || maze_map[new_y][new_x] == 'O'){ //cambio
+    if(maze_map[new_y][new_x] == ' ' || maze_map[new_y][new_x] == 'O'){
         if(!cur_thread){
             // The current thread moves one step forward/left/right without creating a new thread
             *cur_thread = true;
