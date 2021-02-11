a31056e4ed3c0066d7755a95f3e4f9d5abe7a5b4
APVGITHUB <alex.pr434@gmail.com>
2021-02-11 11:16:26 +0100

    update utils

---
+++ utils.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+void walkInsideMaze(int y, int x);
+void printMaze();
+int get_maze(int* in_x, int* in_y,int row,int col);
+void wayAhead(bool* currentThread, int* nextY, int* nextX, vector<vector<char>> maze_map, int newWayY, int newWayX);
