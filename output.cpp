75e01f465262b8823f9f7f6678db3120a967e81d
APVGITHUB <alex.pr434@gmail.com>
2021-02-11 11:50:20 +0100

    add output

---
+++ output.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <thread>
+#include <vector>
+#include "utils.h"
+using namespace std;
+
+extern vector<vector<char>> maze_map;
+extern bool found_exit;
+extern int row,col;
+
+void output(){
+
+    int i,j;
+    while(!found_exit){
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		
+        for(i=0; i < row; i++){
+            for(j=0; j< col; j++){
+                std::cout << maze_map[i][j];
+            }
+            std::cout << "\n";
+        }
+        std::cout << "\n\n";
+    }
+}
